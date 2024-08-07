d147f9d500a83af44badb0a42ee0c178fef53b9e
baekjim <baekjim@duse.co.kr>
2024-08-07 16:17:21 +0900

    add test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function user_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
